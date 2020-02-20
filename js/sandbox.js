861da19f4ab3bb3ab6e3fce6f7073da0da5ffd1c
shaunettDaley <54638418+shaunettDaley@users.noreply.github.com>
2020-02-20 01:36:08 -0500

    Add files via upload

---
+++ js/sandbox.js
@@ -0,0 +1,18 @@
+let cta1 = document.querySelector('.cta1');
+let cta2 = document.querySelector('.cta2');
+
+
+document.addEventListener('scroll', () => {
+    let scrollPosition = window.scrollY;
+
+    if(scrollPosition > 50){
+        cta1.style.bottom = "25%";
+        cta2.style.bottom = "25%";
+    }else{
+        cta1.style.bottom = "0px";
+        cta2.style.bottom = "0px";
+    }
+
+
+})
+
